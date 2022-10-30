2b93ec7e1723a6171b11af61963fe21e364506dc
Kisyok Indran Veerasamy <kisoindran@gmail.com>
2022-10-30 18:54:01 +0800

    Update Footer.vue

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -1,56 +1,64 @@
 <template>
-  <v-footer class="px-0 mt-10" color="transparent">
-    <v-row no-gutters>
-      <div>
-        <v-btn
-              v-for="link in links"
-              :key="link.link"
-              color="primary"
-              text
-              :href="link.link"
-              class="text-capitalize ml-1 font-weight-regular"
+  <div>
+    <v-footer class="px-0 mt-10" color="transparent">
+      <v-row no-gutters>
+        <div>
+          <v-btn
+            v-for="link in links"
+            :key="link.link"
+            color="primary"
+            text
+            :href="link.link"
+            class="text-capitalize ml-1 font-weight-regular"
           >
             {{ link.text }}
           </v-btn>
-      </div>
+        </div>
+        <div>
+          <v-btn
+            v-for="icon in icons"
+            :key="icon.icon"
+            :href="icon.link"
+            class="mr-1"
+            :color="config.light.textColor"
+            icon
+          >
+            <v-icon size="24px">{{ icon.icon }}</v-icon>
+          </v-btn>
+        </div>
+      </v-row>
+    </v-footer>
+    <v-divider></v-divider>
+    <v-row no-gutters class="justify-center mt-3">
       <div>
-        <v-btn
-                v-for="icon in icons"
-                :key="icon.icon"
-                :href="icon.link"
-                class="mr-1"
-                :color=config.light.textColor
-                icon
-        >
-          <v-icon size="24px">{{ icon.icon }}</v-icon>
-        </v-btn>
+        <p class="font-weight-light">
+          iLab &copy; Copyright 2022 All Rights Reserved.
+        </p>
       </div>
     </v-row>
-  </v-footer>
+  </div>
 </template>
 
 <script>
-import config from '../../config';
+import config from "../../config";
 
 export default {
-  name: 'Footer',
+  name: "Footer",
 
   data: () => ({
     config,
     icons: [
-      {icon: 'mdi-facebook', link: 'https://twitter.com/flatlogic/'},
-      {icon: 'mdi-twitter', link: 'https://www.facebook.com/flatlogic'},
-      {icon: 'mdi-github', link: 'https://github.com/flatlogic/'},
-
+      { icon: "mdi-facebook", link: "https://twitter.com/flatlogic/" },
+      { icon: "mdi-twitter", link: "https://www.facebook.com/flatlogic" },
+      { icon: "mdi-whatsapp", link: "https://github.com/flatlogic/" },
     ],
     links: [
-      {text: 'Flatlogic', link: 'https://flatlogic.com/'},
-      {text: 'About Us', link: 'https://flatlogic.com/about'},
-      {text: 'Blog', link: 'https://flatlogic.com/blog'},
-    ]
+      { text: "About Us", link: "https://flatlogic.com/" },
+      { text: "Contact Us", link: "https://flatlogic.com/about" },
+      { text: "FAQ", link: "https://flatlogic.com/blog" },
+    ],
   }),
-}
+};
 </script>
 
-<style src="./Footer.scss" lang="scss"/>
-
+<style src="./Footer.scss" lang="scss" />

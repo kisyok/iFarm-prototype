6c78af4ff1aeb07d1850dd851146ee869957ad45
Kisyok Indran Veerasamy <kisoindran@gmail.com>
2022-10-31 20:43:16 +0800

    aerating (comp)

--- src/pages/Prototypes/Aerating.vue
+++ src/pages/Prototypes/Aerating.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    {{numOfRow}}
     <v-card class="my-3">
       <v-card-title>
         Aerating
@@ -9,12 +10,13 @@
           color="error"
           @click="openForm()"
           v-if="selected.length !== 0"
+          :class="{'btn-delete': $vuetify.breakpoint.xs}"
         >
           Delete Selected Rows
           <v-icon dark right> mdi-delete </v-icon>
         </v-btn>
         <v-spacer></v-spacer>
-        <v-btn large color="primary" @click="openForm()">
+        <v-btn large color="primary" @click="addDialog = true" :class="{'btn-add': $vuetify.breakpoint.xs}">
           Add New Activity
           <v-icon dark right> mdi-plus </v-icon>
         </v-btn>
@@ -78,13 +80,13 @@
                     prepend-icon="mdi-calendar-blank"
                   ></v-text-field>
                 </v-col>
-                <v-col cols="12" sm="4">
+                <v-col cols="6" sm="4">
                   <v-text-field
                     v-model="itemInEdit.field"
                     label="Field Number"
                   ></v-text-field>
                 </v-col>
-                <v-col cols="12" sm="4">
+                <v-col cols="6" sm="4">
                   <v-text-field
                     v-model="itemInEdit.row"
                     label="Row Number"
@@ -103,6 +105,64 @@
           </v-btn>
 
           <v-btn color="blue darken-1" text @click="editDialog = false">
+            Save
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+    <!-- add dialog -->
+    <v-dialog v-model="addDialog">
+      <v-card>
+        <v-toolbar flat color="primary" dark class="d-flex justify-center">
+          <v-toolbar-title>Add New Aerating Activity</v-toolbar-title>
+        </v-toolbar>
+        <v-card-text>
+          <v-container>
+            <v-form ref="addForm">
+              <v-row align="center" class="mt-2">
+                <v-col cols="12" sm="4">
+                  <v-text-field
+                    v-model="numOfRow"
+                    label="Number of Rows to add"
+                    type="number"
+                    min="1"
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+              <v-row align="center" class="mt-2 form-row" v-for="n in 3" :key="n">
+                <v-col cols="12" sm="4">
+                  <v-text-field
+                    v-model="itemInEdit.date"
+                    label="Date"
+                    prepend-icon="mdi-calendar-blank"
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="6" sm="4">
+                  <v-text-field
+                    v-model="itemInEdit.field"
+                    label="Field Number"
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="6" sm="4">
+                  <v-text-field
+                    v-model="itemInEdit.row"
+                    label="Row Number"
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+            </v-form>
+          </v-container>
+        </v-card-text>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+
+          <v-btn color="blue darken-1" text @click="addDialog = false">
+            Cancel
+          </v-btn>
+
+          <v-btn color="blue darken-1" text @click="addDialog = false">
             Save
           </v-btn>
         </v-card-actions>
@@ -173,15 +233,27 @@ export default {
       ],
       search: "",
       editDialog: false,
-      itemInEdit: null,
+      itemInEdit: {id: null, date: null, field: null, row: null},
+      addDialog: false,
+      numOfRow: 1,
     };
-  },
-  methods: {
-    openForm() {
-      console.log();
-    },
   },
 };
 </script>
 
-<style></style>
+<style scoped>
+.btn-delete {
+  margin-bottom: 10px;
+  width: 100%;
+}
+
+.btn-add {
+  margin-bottom: 10px;
+  width: 100%;
+}
+
+.form-row {
+  background-color: rgb(240, 255, 242);
+  margin-bottom: 10px;
+}
+</style>
